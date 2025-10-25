07ee8f58a9a336a9ca9274738e657fcb39597ad4
Shobhit Nagpal <74096450+Shobhit-Nagpal@users.noreply.github.com>
2025-10-25 16:24:52 +0530

    v0.1.0 (#1)
    * chore: change package version
    
    * refactor: add utils
    
    * refactor: main program to generate favicon
    
    * chore: update and install deps
    
    * chore: add jsdoc
    
    * chore: change project name
    
    * refactor: pass output filename as flag
    
    * refactor: change package.json to make it publish ready
    
    * chore: update lock file
    
    * refactor: add shebang on entry file
    
    * refactor: add publish scripts

--- src/index.js
+++ src/index.js
@@ -1,1 +1,47 @@
-console.log('Hello, world')
+#!/usr/bin/env node
+
+import { icons } from "lucide";
+import { program } from "commander";
+import sharp from "sharp";
+import toIco from "to-ico";
+import * as fs from "fs";
+import { ensureFileName, generateSvgString, transformIconName } from "./utils.js";
+
+program
+  .name("mkfav")
+  .description("Convert lucide icons to favicons")
+  .version("0.1.0")
+  .requiredOption("-i, --icon <type>", "Icon to convert")
+  .option("-o, --output <type>", "Output file name");
+
+program.parse();
+
+async function generateFavicon() {
+  try {
+    const options = program.opts();
+    const iconName = transformIconName(options.icon);
+    const outputFileName = ensureFileName(options.output);
+
+    const icon = icons[iconName];
+    if (icon === undefined) {
+      console.error("Icon does not exist");
+      process.exit(1);
+    }
+
+    const svgIconString = generateSvgString(icon);
+
+    const pngBuffer = await sharp(Buffer.from(svgIconString))
+      .resize(32, 32)
+      .png()
+      .toBuffer();
+    const icoBuffer = await toIco([pngBuffer]);
+    fs.writeFileSync(outputFileName, icoBuffer);
+
+    console.log("Icon generated!");
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+}
+
+generateFavicon();
---
+++ src/utils.js
@@ -0,0 +1,86 @@
+/**
+ * Converts kebab-case icon name to PascalCase for Lucide icon lookup
+ * @param {string} name
+ * @returns {string}
+ * @example
+ * transformIconName('folder-git-2') // returns FolderGit2
+ */
+export function transformIconName(name) {
+  const nameSplit = name.split("-");
+  return nameSplit.map((section) => capitalize(section)).join("");
+}
+
+/**
+ * Ensures output filename is correct
+ * @param {string} name
+ * @returns {string}
+ */
+export function ensureFileName(filename) {
+  if (filename === undefined || filename.length === 0) {
+    return "icon.ico";
+  }
+
+  const isIcoSuffixPresent = filename.endsWith(".ico");
+
+  return isIcoSuffixPresent ? filename : `${filename}.ico`;
+}
+
+/**
+ * Capitalizes first letter of a word
+ * @param {string} word
+ * @returns {string}
+ */
+function capitalize(word) {
+  if (word.length === 0) {
+    return word;
+  }
+
+  if (word.length === 1) {
+    return word.toUpperCase();
+  }
+
+  return word[0].toUpperCase() + word.slice(1, word.length);
+}
+
+/**
+ * Generates an svg string from Lucide's IconNode
+ * @param {IconNode} icon
+ * @returns {string}
+ */
+export function generateSvgString(icon) {
+  const tags = [];
+  for (const element of icon) {
+    const tag = generateTagFromElement(element);
+    tags.push(tag);
+  }
+  return `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">\n${tags.join("\n")}\n</svg>`;
+}
+
+/**
+ * Generates HTML tag from an IconNode element
+ * @param {[string, Record<string, string>]} element
+ * @returns {string}
+ * @example
+ * generateTagFromElement(['circle', { cx: '12', cy: '13', r: '2' }])
+ * // returns '<circle cx="12" cy="13" r="2" />'
+ */
+function generateTagFromElement(element) {
+  const tagName = element[0];
+  const attributes = element[1];
+  const transformedAttributes = transformAttributes(attributes);
+  return `<${tagName} ${transformedAttributes} />`;
+}
+
+/**
+ * Transforms attributes object to HTML attributes
+ * @param {Record<string, string>} attributes
+ * @returns {string}
+ */
+function transformAttributes(attributes) {
+  const transformedAttributes = [];
+  for (const prop in attributes) {
+    transformedAttributes.push(`${prop}="${attributes[prop]}"`);
+  }
+
+  return transformedAttributes.join(" ");
+}
